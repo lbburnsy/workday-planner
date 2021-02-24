987bcfb36bfa52d62af2e7fa159c2c5d9bf7bab1
Luc Burns <burnsluc2@gmail.com>
2021-02-24 09:08:00 -0500

    Added save and display task functions.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const saveBtn = $(".saveBtn");
 const textarea = $(".description");
 
+let storedTasks = [];
 let hour = moment().format("HH");
-console.log(hour);
+console.log(storedTasks);
 
 
 // Checks time based on moment, and applies appropriate class styling to the textarea
@@ -19,4 +20,30 @@ function checkTime() {
     }
 }
 
-$(document).ready(checkTime());
+// Function saves each task in each textarea, and sets it to local storage, then resets the array.
+function saveTask() {
+    saveBtn.on("click", ()=> {
+        for (let i = 0; i < textarea.length; i++) {
+            let task = textarea[i].value;
+            storedTasks.push(task);
+            localStorage.setItem("savedTasks", JSON.stringify(storedTasks));
+        }
+        storedTasks = [];
+    })
+}
+
+function displayTasks() {
+    let savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
+    console.log(savedTasks);
+    for (let i = 0; i < textarea.length; i++) {
+        textarea[i].textContent = savedTasks[i];
+    }
+}
+
+function init() {
+    checkTime();
+    displayTasks();
+    saveTask();
+}
+
+$(document).ready(init());
